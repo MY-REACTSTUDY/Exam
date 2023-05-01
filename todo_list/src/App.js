cb669b9c9e0af697110b1ed97a01c2a0784b6961
lsh <tmdghksgkqru@naver.com>
2023-05-01 16:01:24 +0900

    todoList 예제 추가

---
+++ todo_list/src/App.js
@@ -0,0 +1,9 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Ex01TodoList from "./compo/Ex01TodoList";
+
+function App() {
+  return <Ex01TodoList></Ex01TodoList>;
+}
+
+export default App;
